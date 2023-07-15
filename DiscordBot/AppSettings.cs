0dd4284f66871264d05c5a307be68c1718fb2a68
Canberk Bekiroglu <boom.canberk@gmail.com>
2023-07-15 13:08:43 +0300

    First Upload

---
+++ DiscordBot/AppSettings.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    public class AppSettings
+    {
+        public String? ApiKey { get; set; }
+    }
+}
---
+++ DiscordBot/Program.cs
@@ -0,0 +1,40 @@
+using System.Reflection;
+using Discord;
+using Discord.WebSocket;
+using Newtonsoft.Json;
+
+namespace DiscordBot
+{
+    public class Program
+    {
+        public static Task Main(string[] args) => new Program().MainAsync();
+
+        private DiscordSocketClient? _client;
+        private readonly string? _path = $@"{Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.FullName}\";
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+
+            // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
+            // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
+            // var token = File.ReadAllText("token.txt");
+            var token = JsonConvert.DeserializeObject<AppSettings>(File.ReadAllText(_path + "appSettings.json"))?.ApiKey;
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
